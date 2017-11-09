73f723b1e348751b0a6fe062c28ae7bd6adbe0d2
Derek Brooks <derek@broox.com>
2017-11-09 14:35:47 -0600

    update pypi package

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-  name = 'pynuheat',
-  packages = ['pynuheat'],
+  name = 'nuheat',
+  packages = ['nuheat'],
   version = '0.1.1',
   description = 'A Python library that allows control of connected radiant floor thermostats.',
   author = 'Derek Brooks',
   author_email = 'derek@broox.com',
   url = 'https://github.com/broox/python-nuheat',
+  license = 'MIT'
   keywords = ['nuheat', 'thermostat', 'home automation', 'python'],
   classifiers = [],
 )
