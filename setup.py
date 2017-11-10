807ea8304a263d577ea0a1e15c5202beebb8db44
Derek Brooks <derek@broox.com>
2017-11-10 15:48:29 -0600

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'nuheat',
   packages = ['nuheat'],
-  version = '0.1.2',
+  version = '0.2.0',
   description = 'A Python library that allows control of connected NuHeat Signature radiant floor thermostats.',
   author = 'Derek Brooks',
   author_email = 'derek@broox.com',
   url = 'https://github.com/broox/python-nuheat',
-  download_url = 'https://github.com/broox/python-nuheat/archive/0.1.2.tar.gz',
+  download_url = 'https://github.com/broox/python-nuheat/archive/0.2.0.tar.gz',
   license = 'MIT',
   keywords = ['nuheat', 'thermostat', 'home automation', 'python'],
   classifiers = [
